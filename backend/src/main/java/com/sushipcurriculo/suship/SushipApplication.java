b2963c7cc83614246888c9020b0b423f3077abef
Pedro Botter <pedrobotter2016@gmail.com>
2022-09-08 09:50:05 -0300

    Projeto criado

---
+++ backend/src/main/java/com/sushipcurriculo/suship/SushipApplication.java
@@ -0,0 +1,13 @@
+package com.sushipcurriculo.suship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushipApplication.class, args);
+	}
+
+}
